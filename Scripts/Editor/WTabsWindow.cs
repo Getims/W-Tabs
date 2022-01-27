ff0636cff73eb105f5f49a300145a70b273c8447
Getims <57002935+Getims@users.noreply.github.com>
2022-01-27 14:32:14 +0300

    Version 0.1.4

--- Scripts/Editor/WTabsWindow.cs
+++ Scripts/Editor/WTabsWindow.cs
@@ -81,7 +81,7 @@ namespace WTabs
             }
             EditorGUILayout.EndVertical();
 
-            GUILayout.Label("version 0.1.3", centeredLabelBold);
+            GUILayout.Label("version 0.1.4", centeredLabelBold);
         }
 
         private static void CreateMeta()
